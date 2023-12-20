204d2c95e5881980c2798f76cae7106f37519841
techinezz <y.martinez.tech@gmail.com>
2023-12-20 15:26:29 -0500

    Pokemon Card Update
    updated colors for cards and styled it. Also added the pokedex number into the card info

--- src/pages/pokemon/[pokemon].js
+++ src/pages/pokemon/[pokemon].js
@@ -17,17 +17,47 @@ function Pokemon() {
     }, []);
 
 
-    let myLink = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+    let myLink = "";
 
     // This is just a for back up if the pokemon does not have a home image
     if(pokemonItem?.sprites?.other) {
         const  { "official-artwork": link } = pokemonItem?.sprites?.other;
         myLink = link.front_default;
     }
+    //Colors for the pokemon cards
+    const cardColors = [
+        '#FF5733', // Pomegranate
+        '#DAF7A6', // Lime
+        '#FFC300', // Saffron
+        '#581845', // Plum
+        '#C70039', // Red
+        '#900C3F', // Ruby
+        '#6C3483', // Amethyst
+        '#1B4F72', // Ocean Blue
+        '#17202A', // Night
+        '#F4D03F', // Yellow
+        '#2ECC71', // Emerald
+        '#3498DB', // Peter River
+        '#E74C3C', // Alizarin
+        '#9B59B6', // Amethyst
+        '#34495E', // Wet Asphalt
+        '#16A085', // Green Sea
+        '#27AE60', // Nephritis
+        '#2980B9', // Belize Hole
+        '#8E44AD', // Wisteria
+        '#2C3E50', // Midnight Blue
+        '#F39C12', // Orange
+        '#D35400', // Pumpkin
+        '#C0392B', // Pomegranate
+        '#BDC3C7', // Silver
+        '#7F8C8D'  // Asbestos
+    ];
+
+    const randomColor = cardColors[Math.floor(Math.random() * cardColors.length)];
 
     // *This div will display the image of the pokemon when you click on the pokemon card */
     return (
-        <div>
+        <div className={styles.PokemonBg}>
             {pokemonItem && (
             <>
                 <div className={styles.PokemonImage}>
@@ -47,7 +77,10 @@ function Pokemon() {
                                 {pokemonItem?.types?.map((type) => {
                                     return <p key={type.type.name}>{type.type.name}</p>;
                                 })}
-                            
+                        </div>
+                        <div className={styles.PokemonInfoItem}>
+                            <h5>NO.</h5>
+                            <p>{pokemonItem?.id}</p>
                         </div>
                     </div>
                 </div>
